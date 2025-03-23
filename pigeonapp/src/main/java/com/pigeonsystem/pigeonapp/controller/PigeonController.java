37ea09b7a1408e509b9c2b6fabdc3f50e710d1ba
marioantony <mario1995304@gmail.com>
2025-03-23 21:49:01 +0530

    pideon edit done

--- pigeonapp/src/main/java/com/pigeonsystem/pigeonapp/controller/PigeonController.java
+++ pigeonapp/src/main/java/com/pigeonsystem/pigeonapp/controller/PigeonController.java
@@ -3,10 +3,11 @@ package com.pigeonsystem.pigeonapp.controller;
 import com.pigeonsystem.pigeonapp.model.Pigeon;
 import com.pigeonsystem.pigeonapp.model.Users;
 import com.pigeonsystem.pigeonapp.repository.PigeonRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.pigeonsystem.pigeonapp.repository.UserRepository;
 import com.pigeonsystem.pigeonapp.dto.PigeonRequest;
-
+import org.springframework.web.server.ResponseStatusException;
 
 
 import java.util.List;
@@ -38,4 +39,21 @@ public class PigeonController {
     public List<Pigeon> getAll() {
         return pigeonRepository.findAll();
     }
+
+    @PutMapping("/{id}")
+    public Pigeon update(@PathVariable Long id, @RequestBody PigeonRequest request) {
+        Pigeon pigeon = pigeonRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Pigeon not found"));
+
+
+        pigeon.setBloodLine(request.getBloodLine());
+        pigeon.setColor(request.getColor());
+        pigeon.setAge(request.getAge());
+
+        Users owner = userRepository.findById(request.getOwnerId())
+                .orElseThrow(() -> new RuntimeException("Owner not found"));
+        pigeon.setOwner(owner);
+
+        return pigeonRepository.save(pigeon);
+    }
+
 }
